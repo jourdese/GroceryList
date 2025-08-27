c349028c3dce62b9df6c09ff5d9eba7ba286e547
Jourdese Palacio <jourdesepalacio@gmail.com>
2025-08-27 15:04:00 +0800

    Initial Commit

---
+++ GroceryList/GroceryListApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryListApp.swift
+//  GroceryList
+//
+//  Created by Jourdese Palacio on 8/27/25.
+//
+
+import SwiftUI
+
+@main
+struct GroceryListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
